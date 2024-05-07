60b9bdb1672a240022c132101afdda4c003402d2
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-05-07 19:07:20 -0400

    inm.

---
+++ workflow/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="navbar-header-top">
+    <ul>
+      <router-link class="linknav" to="/">Techtack-Technologies</router-link>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.navbar-header-top {
+  padding: 25px;
+  background-color: rgb(23, 37, 51);
+}
+.linknav {
+  margin: 8px;
+  color: white;
+  text-decoration: none;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: 26px;
+}
+
+.linknav:hover {
+  color: lightslategray;
+}
+</style>
---
+++ workflow/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import Navbar from '../components/Navbar.vue'
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <div class="header-content">
+      <div class="header-text">
+        <p id="title">Techtack-Technologies | Workflow</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eveniet quas
+          consectetur incidunt voluptatum eius tenetur culpa rem, magni quasi dolorum veniam,
+          expedita possimus cumque blanditiis est. Eaque, ut cum?
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-content {
+  padding: 125px;
+  background-color: rgb(19, 46, 85);
+}
+
+.header-text #title {
+  color: white;
+  font-family: sans-serif;
+  font-size: 26px;
+  text-align: center;
+}
+
+.header-text p {
+  color: lightgray;
+  font-family: sans-serif;
+  font-size: 22px;
+  text-align: center;
+}
+</style>
